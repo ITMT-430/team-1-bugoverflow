18e74569916611e2f3c60a6a2a81099def49fbfe
neil <nokhandi@hawk.iit.edu>
2016-04-03 21:08:04 -0500

    added the signup page with session login
    currently only accepts
    user= admin
    pass= leech
    as a valid user.
    
    I also added a stupid example of how to handle login

--- flask/test.py
+++ flask/test.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import url_for, flash, redirect, session
 from os import listdir
 
 app = Flask(__name__)
 
+## This thing is supposed to be secret
+## ~~ nyaa ~~
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 @app.route('/',methods=['POST','GET'])
-def process_form():
+#def process_form():
+def index():
+    if 'username' in session:
+        print session['username']
     if request.method == 'POST':
        form_input = request.form['name']
        return render_template('index.html',name=form_input)
@@ -16,18 +23,24 @@ def process_form():
 
        return render_template('index.html',entries=entries)
 
-@app.route('/signup', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST'])
 def login():
     error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin':
-            error = 'Invalid username'
-        elif request.form['password'] != 'leech':
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('signup.html', error=error)
+    message = "" 
+    if request.form['username'] != 'admin':
+        message = 'Invalid username'
+    elif request.form['password'] != 'leech':
+        message = 'Invalid password'
+    else:
+        session['username'] = request.form['username']
+        session['logged_in'] = True
+        print session['username']
+        flash('You were logged in')
+    return redirect(url_for('index'))
+        
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    ##TODO
+    pass
 if __name__ == '__main__':
    app.run(debug=True)
