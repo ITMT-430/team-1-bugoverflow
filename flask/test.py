e4b3d1af29b67985589a95eaee5223e24db23087
neil <nokhandi@hawk.iit.edu>
2016-04-03 23:13:05 -0500

    bug-comment page is now autogenerated

--- flask/test.py
+++ flask/test.py
@@ -6,14 +6,16 @@ from os import listdir
 
 app = Flask(__name__)
 
+bugpath = "imgs/bugs/"
+
 ## This thing is supposed to be secret
 ## ~~ nyaa ~~
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 @app.route('/',methods=['GET'])
 #def process_form():
 def index():
-    imgnames = list(listdir('static/imgs'))
-    entries = [(dict(imagepath="imgs/"+name, link='#')) for name in imgnames]
+    imgnames = list(listdir('static/' + bugpath))
+    entries = [(dict(imagepath=bugpath+name, link="bug/"+name)) for name in imgnames]
 
     return render_template('index.html',entries=entries)
 
@@ -38,10 +40,20 @@ def signup():
     pass
 
 
-@app.route('/bug', methods=['GET', 'POST'])
-def bug():
+@app.route('/bug/<path:path>', methods=['GET', 'POST'])
+def bug(path):
+    tags = ['ladybug', 'beetle', 'spotted']
+    question = "What bug is this??"
+    bug_image = bugpath+path
+
+    return render_template('bug.html',
+            tags = tags,
+            question = question,
+            bug_image = bug_image)
     # [tags]
     # question
+    # bug_image (the name of it)
+
     ## TODO
     pass
 
