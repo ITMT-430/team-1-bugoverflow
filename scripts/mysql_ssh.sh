1f3af8d63597175187df41225f6991771383731a
neil <nokhandi@hawk.iit.edu>
2016-04-04 10:10:55 -0500

    added local encrypted copy of key

--- scripts/mysql_ssh.sh
+++ scripts/mysql_ssh.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 ​
-cat mysql_remove.sh  | ssh -i ~/.ssh/EUCA-BUG-OVERFLOW.pem root@64.131.111.27
-cat mysql_install.sh | ssh -i ~/.ssh/EUCA-BUG-OVERFLOW.pem root@64.131.111.27
+cat mysql_remove.sh  | ssh -i EUCA-BUG-OVERFLOW.pem root@64.131.111.27
+cat mysql_install.sh | ssh -i EUCA-BUG-OVERFLOW.pem root@64.131.111.27
