e6eef429625bfd52d7369c6918aab8822cda5a1a
neil <nokhandi@hawk.iit.edu>
2016-04-03 22:06:24 -0500

    macrofied signup

--- flask/test.py
+++ flask/test.py
@@ -9,19 +9,13 @@ app = Flask(__name__)
 ## This thing is supposed to be secret
 ## ~~ nyaa ~~
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-@app.route('/',methods=['POST','GET'])
+@app.route('/',methods=['GET'])
 #def process_form():
 def index():
-    if 'username' in session:
-        print session['username']
-    if request.method == 'POST':
-       form_input = request.form['name']
-       return render_template('index.html',name=form_input)
-    else:
-       imgnames = list(listdir('static/imgs'))
-       entries = [(dict(imagepath="imgs/"+name, link='#')) for name in imgnames]
+    imgnames = list(listdir('static/imgs'))
+    entries = [(dict(imagepath="imgs/"+name, link='#')) for name in imgnames]
 
-       return render_template('index.html',entries=entries)
+    return render_template('index.html',entries=entries)
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -34,12 +28,12 @@ def login():
     else:
         session['username'] = request.form['username']
         session['logged_in'] = True
-        print session['username']
-        flash('You were logged in')
+        flash('login succeeded')
     return redirect(url_for('index'))
         
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    return render_template('signup.html')
     ##TODO
     pass
 if __name__ == '__main__':
