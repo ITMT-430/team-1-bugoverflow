7004b81ecfcd0049cf433dffd6d71e374e70d603
Zubin D Fanibanda <z.fanibanda@yahoo.com>
2016-04-29 15:51:33 -0500

    changed paths to refeclt actual locations of files

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/ITMT-430/team-1-bugoverflow /team-1-bugoverflow
-gpg --passphrase leech -o /root/EUCA-BUG-OVERFLOW.pem -d /team-1-bugoverflow/scripts/EUCA-BUG-OVERFLOW.pem.gpg
+git clone https://github.com/ITMT-430/team-1-bugoverflow /
+gpg --passphrase leech -o /team-1-bugoverflow/scripts/EUCA-BUG-OVERFLOW.pem -d /team-1-bugoverflow/scripts/EUCA-BUG-OVERFLOW.pem.gpg
 chmod 400 /root/EUCA-BUG-OVERFLOW.pem
-scp -oStrictHostKeyChecking=no -i EUCA-BUG-OVERFLOW.pem /root/EUCA-BUG-OVERFLOW.pem root@64.131.111.80:/root/EUCA-BUG-OVERFLOW.pem
+scp -oStrictHostKeyChecking=no -i /team-1-bugoverflow/scripts/EUCA-BUG-OVERFLOW.pem /root/EUCA-BUG-OVERFLOW.pem root@64.131.111.27:/root/EUCA-BUG-OVERFLOW.pem
