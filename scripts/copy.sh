d46b770406814645ca41243277ea2d0d8d7dd048
Zubin D Fanibanda <z.fanibanda@yahoo.com>
2016-04-29 15:53:04 -0500

    forgot chmod path

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -2,5 +2,5 @@
 
 git clone https://github.com/ITMT-430/team-1-bugoverflow /team-1-bugoverflow
 gpg --passphrase leech -o /team-1-bugoverflow/scripts/EUCA-BUG-OVERFLOW.pem -d /team-1-bugoverflow/scripts/EUCA-BUG-OVERFLOW.pem.gpg
-chmod 400 /root/EUCA-BUG-OVERFLOW.pem
+chmod 400 /team-1-bugoverflow/scripts/EUCA-BUG-OVERFLOW.pem
 scp -oStrictHostKeyChecking=no -i /team-1-bugoverflow/scripts/EUCA-BUG-OVERFLOW.pem /root/EUCA-BUG-OVERFLOW.pem root@64.131.111.27:/root/EUCA-BUG-OVERFLOW.pem
