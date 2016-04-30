9821f5f3c00671cc0c9dce4ced422adb54a59fdc
Alfredof10 <alfredofernandez10@gmail.com>
2016-04-30 14:09:17 -0500

    Update mysql_ssh.sh

--- scripts/mysql_ssh.sh
+++ scripts/mysql_ssh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ​
-cat mysql_slave.sh  | ssh -i EUCA-BUG-OVERFLOW.pem root@64.131.111.26
+cat mysql_slave.sh  | ssh -i EUCA-BUG-OVERFLOW.pem root@64.131.111.26 -o StrictHostKeyChecking=no
 sleep 60
-cat mysql_master.sh | ssh -i EUCA-BUG-OVERFLOW.pem root@64.131.111.27
+cat mysql_master.sh | ssh -i EUCA-BUG-OVERFLOW.pem root@64.131.111.27 -o StrictHostKeyChecking=no
 
