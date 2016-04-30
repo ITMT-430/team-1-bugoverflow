6b0d638815a7e2a899dae22259236081c4f63f3a
Alfredof10 <alfredofernandez10@gmail.com>
2016-04-30 10:42:12 -0500

    Update mysql_ssh.sh

--- scripts/mysql_ssh.sh
+++ scripts/mysql_ssh.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 ​
-cat mysql_remove.sh  | ssh -i EUCA-BUG-OVERFLOW.pem root@64.131.111.27
-cat mysql_install.sh | ssh -i EUCA-BUG-OVERFLOW.pem root@64.131.111.27
+cat mysql_slave.sh  | ssh -i EUCA-BUG-OVERFLOW.pem root@64.131.111.26
+sleep 60
+cat mysql_master.sh | ssh -i EUCA-BUG-OVERFLOW.pem root@64.131.111.27
