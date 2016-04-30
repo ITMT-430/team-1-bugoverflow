085172de317f19e4a3ca02572a1d8a2a2457f843
Zubin D Fanibanda <zfanibanda>
2016-04-30 11:47:40 -0500

    changed command setting

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gpg --passphrase leech -o /team-1-bugoverflow/scripts/EUCA-BUG-OVERFLOW.pem -d /team-1-bugoverflow/scripts/EUCA-BUG-OVERFLOW.pem.gpg
+gpg -o /team-1-bugoverflow/scripts/EUCA-BUG-OVERFLOW.pem -d /team-1-bugoverflow/scripts/EUCA-BUG-OVERFLOW.pem.gpg
 chmod 400 /team-1-bugoverflow/scripts/EUCA-BUG-OVERFLOW.pem
 scp -oStrictHostKeyChecking=no -i /team-1-bugoverflow/scripts/EUCA-BUG-OVERFLOW.pem /team-1-bugoverflow/scripts/EUCA-BUG-OVERFLOW.pem root@64.131.111.80:/root/EUCA-BUG-OVERFLOW.pem
