248c4261a1e49b460d04808eb3b3018d91229574
Zubin D Fanibanda <zfanibanda>
2016-05-01 14:56:15 -0500

    adding new version of scripts to repo

---
+++ scripts/killall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+euca-disassociate-address 64.131.111.26
+euca-disassociate-address 64.131.111.27
+euca-disassociate-address 64.131.111.32
+rm /euca2ools/ -R
+rm "/team-1-bugoverflow/" -R
+open="$(euca-describe-instances | grep Team1_Bug | awk '{print $3}')"
+euca-terminate-instances ${open}
+
