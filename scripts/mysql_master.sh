bfd0e3209ee2772a7a88eb8b3e2e009638e5acef
Alfredof10 <alfredofernandez10@gmail.com>
2016-05-01 21:02:00 -0500

    Update mysql_master.sh

--- scripts/mysql_master.sh
+++ scripts/mysql_master.sh
@@ -4,7 +4,7 @@ yum install vim mysql-server -y    		 #Install msql clients/server and git
 #git clone https://github.com/ITMT-430/team-1-bugoverflow #Pull repo
 rm -f /etc/my.cnf					 #Removes SQL config
 touch /var/lib/mysql/mysql-bin.log
-cp /team-1-bugoverflow/Nginx/Master.cnf /etc/my.cnf      #Replace SQL config
+cp /root/team-1-bugoverflow/Nginx/Master.cnf /etc/my.cnf      #Replace SQL config
 mkdir /var/log/mysql					 #Creates log file
 chown -R mysql:mysql /var/log/mysql			 #Grant SQL access log file
 service mysqld start					 #Start SQL
