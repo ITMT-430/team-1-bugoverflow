d2b9dfdaf61a6b05cdbfa8acf68803509db86fa7
Zubin D Fanibanda <zfanibanda>
2016-05-01 23:25:28 -0500

    made changed to incorporate logging

--- scripts/mysql_master.sh
+++ scripts/mysql_master.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 yum install vim mysql-server -y    		 #Install msql clients/server and git
+yum install xinetd -y
+yum install rpcbind -y
 #cd ..							 
 #git clone https://github.com/ITMT-430/team-1-bugoverflow #Pull repo
 rm -f /etc/my.cnf					 #Removes SQL config
@@ -8,6 +10,12 @@ cp /root/team-1-bugoverflow/Nginx/Master.cnf /etc/my.cnf      #Replace SQL confi
 mkdir /var/log/mysql					 #Creates log file
 chown -R mysql:mysql /var/log/mysql			 #Grant SQL access log file
 service mysqld start					 #Start SQL
+service xinetd restart
+service rpcbind restart
+service rsyslog restart
+
+
+
 
 mysqladmin -u root password leech	      		 # root:leech user created
 chkconfig mysqld on            				 # set to start on boot
