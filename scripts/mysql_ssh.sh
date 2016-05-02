ca6d20286636797aff4d8747ac03e1b0423ef219
Alfredof10 <alfredofernandez10@gmail.com>
2016-05-01 21:03:03 -0500

    Update mysql_ssh.sh

--- scripts/mysql_ssh.sh
+++ scripts/mysql_ssh.sh
@@ -2,9 +2,8 @@
 
 cat mysql_slave.sh  | ssh -i EUCA-BUG-OVERFLOW.pem root@64.131.111.26 -o StrictHostKeyChecking=no
 sleep 30
-ssh -i /team-1-bugoverflow/scripts/EUCA-BUG-OVERFLOW.pem root@64.131.111.27 -o StrictHostKeyChecking=no "cd .."
 ssh -i /team-1-bugoverflow/scripts/EUCA-BUG-OVERFLOW.pem root@64.131.111.27 -o StrictHostKeyChecking=no "yum install git -y"
 sleep 30
 ssh -i /team-1-bugoverflow/scripts/EUCA-BUG-OVERFLOW.pem root@64.131.111.27 -o StrictHostKeyChecking=no "git clone https://github.com/ITMT-430/team-1-bugoverflow"
-ssh -i /team-1-bugoverflow/scripts/EUCA-BUG-OVERFLOW.pem root@64.131.111.27 -o StrictHostKeyChecking=no "/bin/bash /team-1-bugoverflow/scripts/mysql_master.sh"
+ssh -i /team-1-bugoverflow/scripts/EUCA-BUG-OVERFLOW.pem root@64.131.111.27 -o StrictHostKeyChecking=no "/bin/bash /root/team-1-bugoverflow/scripts/mysql_master.sh"
 
