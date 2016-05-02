fe510a0891721744f4131b3bdeee1f472210a927
Alfredof10 <alfredofernandez10@gmail.com>
2016-05-01 19:58:43 -0500

    Update web_deploy.sh

--- scripts/web_deploy.sh
+++ scripts/web_deploy.sh
@@ -20,7 +20,7 @@ cd team-1-bugoverflow/flask/
 
 virtualenv flaskenv
 source flaskenv/bin/activate
-pip install uwsgi flask Jinja2 Flask-SQLAlchemy exifread pymysql flask_recaptcha requests re
+pip install uwsgi flask Jinja2 Flask-SQLAlchemy exifread pymysql flask_recaptcha requests
 yum install python-jinja2 -y
 python -c "import mydb; mydb.rebuilddb()"
 deactivate
