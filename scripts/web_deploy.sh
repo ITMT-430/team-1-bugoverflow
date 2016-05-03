4e486d6dcf5d5e541f76f4ec8bd800091e0965ac
Alfredof10 <alfredofernandez10@gmail.com>
2016-05-03 18:14:29 -0500

    separated pip install commands

--- scripts/web_deploy.sh
+++ scripts/web_deploy.sh
@@ -26,13 +26,16 @@ cd /team-1-bugoverflow/flask/
 
 virtualenv flaskenv
 source flaskenv/bin/activate
-pip install uwsgi flask Jinja2 Flask-SQLAlchemy exifread pymysql flask_recaptcha requests Flask-AlchemyDumps
+pip install uwsgi
 pip install flask
 pip install Jinja2
 pip install Flask-SQLAlchemy
-pip install 
-pip install 
-pip install 
+pip install exifread
+pip install pymysql
+pip install flask_recaptcha
+pip install requests
+pip install Flask-AlchemyDumps
+
 yum install python-jinja2 -y
 #cd /team-1-bugoverflow/flask && source flaskenv/bin/activate && 
 python -c "import mydb; mydb.rebuilddb()"
