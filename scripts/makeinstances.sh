516ad16635e6296d81690c1123b1bf4ed101e65c
Zubin D Fanibanda <zfanibanda>
2016-05-03 11:52:57 -0500

    Made final changes and added more descriptive literature

--- scripts/makeinstances.sh
+++ scripts/makeinstances.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#Thanks to Brian Semrau and Team IRL for portions of this script.
 # Usage: Run on vagrant machine or another Debian box as root (not sudo - actually switch to root):
 # bash <(curl -s https://raw.githubusercontent.com/ITMT-430/Team-3-Install-Scripts/master/deploy-environment.sh)
 apt-get install -y git vim eucatools
@@ -21,26 +21,25 @@ echo "source /euca2ools/creds/eucarc">>~/.bashrc
 echo "source /euca2ools/creds/eucarc">>~/.zshrc
 
 euca-version
-echo "Deploying instances now, please wait."
+echo "Deploying instances now. Please wait, this may take some time. Check Lexington for further Info"
 outputWS="$(euca-run-instances emi-b6ecd5f1 -n 1 -k 'BugKey' -g 'BugSecurity' -t m1.xlarge)"
 instanceWS="$(echo "${outputWS}" | grep -o 'i-.\{0,8\}' | head -1)"
 ipadWS="$(euca-describe-instances | grep ${instanceWS} | grep -o '64\.131\.111\..\{0,3\}' | tr -s [:space:])"
 sleep 25
+echo "Webserver launched"
 
 outputDR="$(euca-run-instances emi-b6ecd5f1 -n 1 -k 'BugKey' -g 'BugSQL' -t m1.xlarge)"
 instanceDR="$(echo "${outputDR}" | grep -o 'i-.\{0,8\}' | head -1)"
 ipadDR="$(euca-describe-instances | grep ${instanceDR} | grep -o '64\.131\.111\..\{0,3\}' | tr -s [:space:])"
 sleep 25
+echo "SQL Read launched"
 
 outputDW="$(euca-run-instances emi-b6ecd5f1 -n 1 -k 'BugKey' -g 'BugSQL' -t m1.xlarge)"
 instanceDW="$(echo "${outputDW}" | grep -o 'i-.\{0,8\}' | head -1)"
 ipadDW="$(euca-describe-instances | grep ${instanceDW} | grep -o '64\.131\.111\..\{0,3\}' | tr -s [:space:])"
-sleep 25
-
-outputL="$(euca-run-instances emi-b6ecd5f1 -n 1 -k 'BugKey' -g 'BugSecurity' -t m1.xlarge)"
-instanceL="$(echo "${outputL}" | grep -o 'i-.\{0,8\}' | head -1)"
-ipadL="$(euca-describe-instances | grep ${instanceL} | grep -o '64\.131\.111\..\{0,3\}' | tr -s [:space:])"
-sleep 25
+sleep 35
+echo "SQL Write launched"
+sleep 3
 
 
 echo "wait 10 secs, IPs are being added"
@@ -54,9 +53,7 @@ sleep 3
 newipDW="$(euca-associate-address -i ${instanceDW} 64.131.111.27)"
 echo ${newipDW}
 sleep 3
-newipL="$(euca-associate-address -i ${instanceL} 64.131.111.30)"
-echo ${newipL}
-sleep 3
+
 
 echo "wait 10 secs, Servers are being named"
 sleep 1
@@ -66,8 +63,7 @@ euca-create-tags ${instanceDR} --tag Name=Team1_BugRead
 sleep 3
 euca-create-tags ${instanceDW} --tag Name=Team1_BugWrite
 sleep 3
-euca-create-tags ${instanceL} --tag Name=Team1_BugLog
-sleep 3
+
 
 echo "Instances should be up and running now, check the Eucalyptus web page. Running the next script soon."
 
@@ -75,7 +71,7 @@ sleep 20
 ssh-keygen -f "/root/.ssh/known_hosts" -R 64.131.111.26
 ssh-keygen -f "/root/.ssh/known_hosts" -R 64.131.111.27
 ssh-keygen -f "/root/.ssh/known_hosts" -R 64.131.111.32
-ssh-keygen -f "/root/.ssh/known_hosts" -R 64.131.111.30
+
 
 
 /bin/bash /team-1-bugoverflow/scripts/copy.sh
